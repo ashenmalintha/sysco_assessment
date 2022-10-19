cfe7a5089e92820530121d27144eeaa00acaf431
Your Name <you@example.com>
2022-10-19 14:10:55 +0530

    t

--- infrastructure/modules/state-backend/variables.tf
+++ infrastructure/modules/state-backend/variables.tf
@@ -1,11 +1,11 @@
 variable "state_bucket_name" {
   description = "The name of the state bucket"
   type        = string
-  default     = "demo-eks-state"
+  default     = "demo-eks-state-test0001"
 }
 
 variable "dynamodb_table_name" {
   description = "The name of the dynamodb table"
   type        = string
-  default     = "demo-eks-state-lock"
+  default     = "demo-eks-state-lock-test0001"
 }
